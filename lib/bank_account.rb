fc81f84a7c16e1a2cf860444f3c0ff00df99f1cd
Raz Al-jaf <razaljaf@gmail.com>
2021-01-03 10:48:41 -0700

    commit tree 8ec89f289acd7ad0ef4e5597a8c631163099e6da

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,15 @@
-class BankAccount
+require 'pry'
 
+class BankAccount
+    # @@all = []
+    attr_accessor :name, :balance, :status
+    def initialize name, balance, status
+            @name = name
+            @balance = balance
+            @status = status
+            # @@all << self
+    end
+
+    avi = BankAccount.new "Avi", 1000, "Valid"
 end
+# binding.pry
